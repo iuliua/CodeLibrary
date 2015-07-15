af2872563d0fb309fb1b4ee4a20f919d5f9a786d
iuliua <iuliua@gmail.com>
2015-07-15 15:41:22 +1000

    * DoubleNotEqual

--- Tools/DoubleComparison.h
+++ Tools/DoubleComparison.h
@@ -5,6 +5,10 @@ namespace Tools
     {
         return (abs(d1 - d2) < DBL_EPSILON);
     }
+    inline static bool DoubleNotEqual(double d1, double d2)
+    {
+        return !DoubleEqual(d1, d2);
+    }
     inline static bool DoubleNotZero(double d1)
     {
         return !DoubleEqual(d1, 0.0);
