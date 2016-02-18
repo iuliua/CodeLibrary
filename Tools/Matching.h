ef84eb49e1a78aac4d66bca55f959eacea12e502
iuliua <iuliua@gmail.com>
2016-02-18 13:01:52 +1100

    * improved matching * added negative

--- Tools/Matching.h
+++ Tools/Matching.h
@@ -17,15 +17,23 @@ private:
 
     bool MatchWild(const std::string &pattern, const std::string &input)
     {
-        if (pattern[pattern.size() - 1] == '*' && pattern[pattern.size() - 2] == '\\')
+        if (pattern[pattern.size() - 1] == '*')
         {
+            if (pattern.size() == 1)
+                return true;
             std::string pattern_path = pattern.substr(0, pattern.length() - 1);
-            if (pattern_path.size() > 0)
-            {
-                std::string symb_path(input);
-                if (symb_path.find(pattern_path) == 0)
-                    return true;
-            }
+            if (input.find(pattern_path) == 0)
+                return true;
+        }
+        return false;
+    }
+    bool MatchNegative(const std::string &pattern, const std::string &input)
+    {
+        if (pattern[0] == '!')
+        {
+            std::string pattern_negative = pattern.substr(1);
+            if (!MatchExact(pattern_negative, input) && !MatchWild(pattern_negative, input))
+               return true;
         }
         return false;
     }
@@ -44,7 +52,7 @@ public:
     bool Match(const std::string &input)
     {
         for (size_t i = 0; i < m_pattern_list.size(); i++)
-            if (MatchExact(m_pattern_list[i], input) || MatchWild(m_pattern_list[i], input))
+            if (MatchExact(m_pattern_list[i], input) || MatchWild(m_pattern_list[i], input) || MatchNegative(m_pattern_list[i], input))
                 return true;
         return false;
     }
