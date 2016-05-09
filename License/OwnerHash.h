d4bc77ad304e39db31b091323e2fda8011cf1e83
iuliua <iuliua@gmail.com>
2016-05-09 12:49:04 +1000

    * fixed tabs and added Vantage MT5

--- License/OwnerHash.h
+++ License/OwnerHash.h
@@ -1,52 +1,55 @@
 #ifdef MTServerAPIVersion
-					// "TestOnlyAu"
-   #define TRIAL_END_DATE 0,0,0           
-   #define OWNER_HASH 2583523366 
-					// "AppleWealth" 
+                                        // "TestOnlyAu"
+   #define TRIAL_END_DATE 0,0,0
+   #define OWNER_HASH 2583523366
+                                        // "AppleWealth" 
 // #define TRIAL_END_DATE 2016,4,13
-// #define OWNER_HASH 3988937529 
-					// "KimEng" 
+// #define OWNER_HASH 3988937529
+                                        // "KimEng" 
 // #define TRIAL_END_DATE 2016,4,13
-// #define OWNER_HASH 808574437  
+// #define OWNER_HASH 808574437
+                                        // "VantageFX" 
+// #define TRIAL_END_DATE 2016,6,9
+// #define OWNER_HASH 405379118
 
 #endif
 
 
-#ifdef ServerApiVersion        
+#ifdef ServerApiVersion
                                         // "Only for test"
    #define TRIAL_END_DATE 0,0,0
    #define OWNER_HASH 1765005115 
-					// "GC Group Limited"
-// #define TRIAL_END_DATE 2016,3,31 
+                                        // "GC Group Limited"
+// #define TRIAL_END_DATE 2016,3,31
 // #define OWNER_HASH 3267069397 
-					// "Bethle Aster Global Investments Limited"
+                                        // "Bethle Aster Global Investments Limited"
 // #define TRIAL_END_DATE 2016,5,10
 // #define OWNER_HASH 2380660658 
-					// "Vantage FX Pty Ltd."
+                                        // "Vantage FX Pty Ltd."
 // #define TRIAL_END_DATE 2016,3,3
 // #define OWNER_HASH 1447832137 
-					// "Forpro Group Pty Ltd"
+                                        // "Forpro Group Pty Ltd"
 // #define TRIAL_END_DATE 2016,5,18
 // #define OWNER_HASH 1807536020 
-					// "Vantage Global Prime Pty Ltd"
+                                        // "Vantage Global Prime Pty Ltd"
 // #define TRIAL_END_DATE 2016,3,3
 // #define OWNER_HASH 2860346951 
-					// "Fullerton Markets Limited"
+                                        // "Fullerton Markets Limited"
 // #define TRIAL_END_DATE 2016,3,16
 // #define OWNER_HASH 3538253584
-					// "UOB Kay Hian Private Limited"
+                                        // "UOB Kay Hian Private Limited"
 // #define TRIAL_END_DATE 2016,5,13
 // #define OWNER_HASH 1191163956
-					// "Golden Bullion (Hong Kong) Company Limited"
-// #define TRIAL_END_DATE 0,0,0      
+                                        // "Golden Bullion (Hong Kong) Company Limited"
+// #define TRIAL_END_DATE 0,0,0
 // #define OWNER_HASH 1532620363
-					// "Evolution iTrade Finance Group Limited"
-// #define TRIAL_END_DATE 0,0,0           
+                                        // "Evolution iTrade Finance Group Limited"
+// #define TRIAL_END_DATE 0,0,0
 // #define OWNER_HASH 1027648337
-					// "Boisil Group Limited"
+                                        // "Boisil Group Limited"
 // #define TRIAL_END_DATE 0,0,0
 // #define OWNER_HASH 3785648753
-					// "TransMarket Group Ltd"
+                                        // "TransMarket Group Ltd"
 // #define TRIAL_END_DATE 0,0,0  
 // #define OWNER_HASH 1625696139
 
